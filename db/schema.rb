53b46966ba63b4c31e4b1e3c95758fb9d8ca6697
denizutku <beydogandeniz@gmail.com>
2021-03-09 17:37:44 +0300

    admin role configuration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_141602) do
+ActiveRecord::Schema.define(version: 2021_03_09_142927) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_141602) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "surname"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
