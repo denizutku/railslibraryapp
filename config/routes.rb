6c6d5c5d25cc46348cdc806d3a198aae9e9b0aac
denizutku <beydogandeniz@gmail.com>
2021-03-09 17:21:54 +0300

    devise user model configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books
   resources :authors
   resources :categories
 
-  root to: "index"
+  root to: "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
